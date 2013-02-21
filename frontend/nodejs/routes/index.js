4fcc813b8513cf721842bf53cee71cfc4928024d
Andrew Hart <andrew@datafluency.com>
2013-02-21 03:41:33 +0000

    improve documentation on top level routes

--- frontend/nodejs/routes/index.js
+++ frontend/nodejs/routes/index.js
@@ -1,8 +1,20 @@
-
-/*
- * GET home page.
+/**
+ * ParkUrban - Mobile Web Application
+ *
+ * Top-level application routes
+ *
+ * This file contains the controller information for all of the top-level 
+ * web application views not otherwise implemented in one of the application
+ * modules.
+ *
+ * @author andrew <andrew@datafluency.com>
+ *
+ **/
+ 
+/**
+ * Application home page
+ *
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ParkUrban' });
 };
