02198ed1511cb43d21334b6c329d809d7307c3f3
Andrew Hart <andrew@datafluency.com>
2013-02-21 03:40:37 +0000

    remove obsolete console.log messages and update documentation

--- frontend/nodejs/auth.js
+++ frontend/nodejs/auth.js
@@ -1,10 +1,13 @@
 /**
- * Passport + MongoDB Authentication
+ * ParkUrban - Mobile Web Application
+ *
+ * Application user authentication
  *
  * Implements the Passport LocalStrategy authentication strategy for
- * authenticating ParkUrban clients.
+ * authenticating ParkUrban clients to the mobile web application.
  *
  * @author andrew <andrew@datafluency.com>
+ *
  **/
 var passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy
@@ -13,51 +16,36 @@ var passport = require('passport')
 
 
 exports.configure = function () {
-    console.log('configure called. passport is');
-    console.log(passport);
-    
-    console.log("Instructing to use new LocalStrategy...");
+
     passport.use(new LocalStrategy({
           usernameField: 'mail',
           passwordField: 'password'
         },
         function(mail, password, done) {
-        console.log('auth.js:local strategy, looking up user...');
         schema.User.findOne({ mail: mail }, function (err, user) {
           if (err) { 
-            console.log('an error occurred...');
             return done(err); 
           }
           if (!user) {
-            console.log('unknown user...');
             return done(null, false, { message: 'Unknown user' });
           }
           if (!user.authenticate(password)) {
-            console.log('invalid password...');
             return done(null, false, { message: 'Invalid password' });
           }
-          console.log('Good user ' + util.inspect(user));
           return done(null, user);
         });
       }
     ));
     
-    console.log("Registering serializeUser...");
     passport.serializeUser(function(user, done) {
-      console.log('called serializeUser (' + user.mail + ')');
       done(null, user.mail);
     });
 
-    console.log("Registering deserializeUser...");
     passport.deserializeUser(function(id, done) {
-      console.log('called deserializeUser (' + id + ')');
       schema.User.findOne({mail: id}, function (err, user) {
         done(err, user);
       });
     });
-    
-    console.log('passport is now: ');
-    console.log(passport);
 };
 
 // Export the main Passport object
